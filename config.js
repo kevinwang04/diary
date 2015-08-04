18e44fdf6caffc23cc9ebb9f8b14aff4edba4177
kevinwang04 <kavinwang04@gmail.com>
2015-08-04 19:47:16 +0800

    08.04

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   author: {
-    name: "Joyee Cheung",
-    url: "https://github.com/joyeecheung"
+    name: "kevinwang",
+    url: "https://github.com/kevinwang04"
   },
   repo: {  // for the markdown source
-    content: "https://github.com/joyeecheung/my-tech-diary",
+    content: "https://github.com/kevinwang04/my-tech-diary",
     as_submodule: 'diary'
   },
   base_url: '/diary',  // remote directory for the website
   ga_id: 'UA-63818301-1',  // Optional: Universal Google Analytics ID
-  title: 'My Tech Diary'
+  title: 'My Diary'
 }
