f577f4b67a4858e3f0d40f2915dfb4c0d5e83c7c
kevinwang04 <kavinwang04@gmail.com>
2015-08-11 20:11:20 +0800

    add 11

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
     url: "https://github.com/kevinwang04"
   },
   repo: {  // for the markdown source
-    content: "https://github.com/kevinwang04/my-tech-diary",
-    as_submodule: 'my-tech-diary'
+    content: "https://github.com/kevinwang04/my-diary.git",
+    as_submodule: 'my-diary'
   },
   base_url: '/diary',  // remote directory for the website
   ga_id: 'UA-63818301-1',  // Optional: Universal Google Analytics ID
